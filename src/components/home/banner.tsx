20112c3d772c240f4ca45c7177526de50579039d
laizaguedes <laiza.lima.guedes@gmail.com>
2025-10-03 07:07:34 -0300

    banner finalizado

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -1,12 +1,41 @@
+"use client"
+
 import { Banner } from "@/types/banner"
 import Image from "next/image"
 import Link from "next/link"
+import { use, useEffect, useState } from "react"
 
 type Props = {
     list: Banner[]
 }
 
+let bannerTimer: NodeJS.Timeout;
+let bannerTime = 5000;
+
 export const Banners = ({ list }: Props) => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const nextImage = () => {
+        setCurrentImage((currentImage: any) => {
+            if (currentImage + 1 >= list.length) {
+                return 0;
+            } else {
+                return currentImage + 1;
+            }
+        });
+    }
+
+    const handleBannerClick = (index: number) => {
+        setCurrentImage(index);
+        clearInterval(bannerTimer);
+        bannerTimer = setInterval(nextImage, bannerTime);
+    }
+
+    useEffect(() => {
+        bannerTimer = setInterval(nextImage, bannerTime);
+        return () => clearInterval(bannerTimer);
+    }, []);
+
     return (
         <div>
             <div className="relative aspect-[3/1]">{/* com a classe relativa é possível trazer todas as imagens uma embaixo da outra utilizando o absolute */}
@@ -15,6 +44,7 @@ export const Banners = ({ list }: Props) => {
                         key={index}
                         href={banner.link}
                         className="transition-all absolute inset-0"
+                        style={{ opacity: currentImage === index ? 1 : 0 }} /* permite deixar só a imagem do currentImage visível */
                     >
                         <Image
                             src={banner.img}
@@ -26,8 +56,17 @@ export const Banners = ({ list }: Props) => {
                     </Link>
                 ))}
             </div>
-            <div className="">
-                ...
+            <div className="mt-4 flex justify-center gap-4">
+                {list.map((banner, index) => (
+                    <div
+                        key={index}
+                        className="size-3 bg-blue-600 rounded-full cursor-pointer"
+                        style={{ opacity: currentImage === index ? 1 : 0.3 }}
+                        onClick={() => handleBannerClick(index)}
+                    >
+
+                    </div>
+                ))}
             </div>
         </div>
     )
